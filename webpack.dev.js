436eccbff43d8c0f81a01c202eec35eb6bbf49f6
zhefengjiang <zhefengjiang@easyops.cn>
2019-08-31 16:25:55 +0800

    feat(): 启用 source-map

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,53 @@
 "use strict"
 
+const glob = require("glob");
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
+const setMPA = () => {
+    const entry = {}
+
+    const htmlWebpackPlugins = [];
+
+    const entryFiles = glob.sync(path.join(__dirname, "./src/*/index.js"));
+
+    Object.keys(entryFiles)
+        .map(index => {
+            const entryFile = entryFiles[index];
+
+            const match = entryFile.match(/src\/(.*)\/index\.js/);
+            const pageName = match && match[1];
+
+            entry[pageName] = entryFile;
+            htmlWebpackPlugins.push(new HtmlWebpackPlugin({
+                template: path.join(entryFile, `../${pageName}.html`),
+                filename: `${pageName}.html`,
+                chunks: [`${pageName}`],
+                inject: true,
+                minify: {
+                    html5: true,
+                    collapseWhitespace: true,
+                    preserveLineBreaks: false,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    removeComments: false
+                }
+            }),)
+        });
+
+
+    return {
+        entry,
+        htmlWebpackPlugins
+    }
+};
+
+const {entry, htmlWebpackPlugins} = setMPA();
 
 module.exports = {
-    entry: {
-        index: "./src/index.js",
-        search: "./src/search.js"
-    },
+    entry: entry,
     output: {
         path: path.join(__dirname, "dist"),
         filename: "[name].js"
@@ -59,6 +97,8 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin()
-    ]
+        new CleanWebpackPlugin(),
+        ...htmlWebpackPlugins
+    ],
+    devtool: "source-map"
 };
