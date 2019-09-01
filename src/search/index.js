ce4827b2286dbfc2cfc3432a0d953100b5e2d84f
zhefengjiang <zhefengjiang@easyops.cn>
2019-09-01 16:15:13 +0800

    feat(): 动态 import

--- src/search/index.js
+++ src/search/index.js
@@ -4,9 +4,31 @@ import "./search.less";
 import logo from "./imags/timg.jpeg";
 
 class Index extends React.Component{
+    constructor() {
+        super(...arguments);
+
+        this.state = {
+            text: ""
+        }
+    }
+
+    loadComponent() {
+        import("./text.js").then((text) => {
+            this.setState({
+                text: text.default
+            })
+        })
+    }
+
  render() {
-     console.log(logo);
-     return <div className="search-text">搜索文字内容<img src={logo}/></div>
+        const { text } = this.state;
+     return (
+         <div className="search-text">
+             {text || ""}
+             搜索文字内容
+             <img src={logo} onClick={this.loadComponent.bind(this)} />
+         </div>
+     )
  }
 }
 
