448426720c629912f0a767677bb9a14991c01d5e
zhefengjiang <zhefengjiang@easyops.cn>
2019-09-01 17:20:39 +0800

    feat(): eslint 与 webpack 的结合

--- webpack.prod.js
+++ webpack.prod.js
@@ -59,7 +59,10 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use: "babel-loader"
+                use: [
+                    "babel-loader",
+                    "eslint-loader"
+                    ]
             },
             {
                 test: /\.css$/,
